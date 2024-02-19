191a68f14f00224d0ef1cd622dc75d499142c192
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-02-19 13:36:11 +0530

    bg data added

---
+++ lib/screens/login/data/bg_data.dart
@@ -0,0 +1,10 @@
+List<String> bgList = [
+  "assets/login/bg1.jpeg",
+  "assets/login/bg2.jpeg",
+  "assets/login/bg3.jpeg",
+  "assets/login/bg4.webp",
+  "assets/login/bg5.jpeg",
+  "assets/login/bg6.jpeg",
+  "assets/login/bg7.jpg",
+  "assets/login/bg8.jpeg",
+];
